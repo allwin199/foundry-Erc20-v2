caa2c6fb812f58b7020a94f0813e043f91be81e5
worksofallwin <princeallwin199@gmail.com>
2023-07-31 23:06:51 +0530

    Complete Deploy scripts and add makefile

---
+++ script/DeployOurToken.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract DeployOurToken is Script {
+    function run() external {
+        uint256 initialSupply = 1000 ether;
+
+        vm.startBroadcast();
+        new OurToken(initialSupply);
+        vm.stopBroadcast();
+    }
+}
